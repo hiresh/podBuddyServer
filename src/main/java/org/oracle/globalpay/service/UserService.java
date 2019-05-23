42d24fa30a05eb0de3e8ac8f55f6b34017483b0b
yvshah <yvshah@YVSHAH-LAP.oradev.oraclecorp.com>
2019-05-23 12:19:33 +0530

    Removed unused imports

--- src/main/java/org/oracle/globalpay/service/UserService.java
+++ src/main/java/org/oracle/globalpay/service/UserService.java
@@ -1,7 +1,5 @@
 package org.oracle.globalpay.service;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
